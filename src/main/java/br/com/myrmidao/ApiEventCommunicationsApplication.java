0d3e84e2800c1c3a6d9da4bddecb900411e05941
LucasMMF <mateusmidao@gmail.com>
2023-04-29 19:47:07 -0300

    build(api): first commit

---
+++ src/main/java/br/com/myrmidao/ApiEventCommunicationsApplication.java
@@ -0,0 +1,13 @@
+package br.com.myrmidao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventCommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventCommunicationsApplication.class, args);
+	}
+
+}
